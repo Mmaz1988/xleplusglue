e705662c69a8d679f59fdc51848581b6551a5c47
mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2025-06-02 10:32:00 +0200

    updated betaConversionTestsuite.pl

--- gswb_resources/boxer/betaConversionTestsuite.pl
+++ gswb_resources/boxer/betaConversionTestsuite.pl
@@ -36,100 +36,100 @@ test(app(lam(X:e,drs([],[pred(sleep,X)])),mary),drs([],[pred(sleep,mary)])).
 
 test(app(lam(X,drs([],[pred(sleep,X)])),mary),drs([],[pred(sleep,mary)])).
 
-% type on discourse referent
-%test(
-%    merge(
-%        drs([Y:e],[]),
-%        app(lam(X,
-%            drs([],[eq(Y,X),pred(sleep,Y)])),
-%        mary)
-%    ),
-%    merge(
-%        drs([Y:e],[]),
-%        drs([],[eq(Y,mary),pred(sleep,Y)])
-%    )
-%).
-
-% some, typed
-test(
-    merge(
-        drs([X:e,Y:e],[]),
-        app(lam(Z,
-            drs([],[eq(X,Z),pred(musician,Y),rel(loves,X,Y)])),
-        john)
-    ),
-    merge(
-        drs([X:e,Y:e],[]),
-        drs([],[eq(X,john),pred(musician,Y),rel(loves,X,Y)])
-    )
-).
-
-% forall, typed
-test(
-    merge(
-        drs([X:e],[]),
-        app(lam(Z,
-            drs([],[eq(X,Z),imp(drs([Y:e],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])),
-        john)
-    ),
-    merge(
-        drs([X:e],[]),
-        drs([],[eq(X,john),imp(drs([Y:e],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])
-    )
-).
-
-% forall, untyped
-test(
-    merge(
-        drs([X],[]),
-        app(lam(Z,
-            drs([],[eq(X,Z),imp(drs([Y],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])),
-        john)
-    ),
-    merge(
-        drs([X],[]),
-        drs([],[eq(X,john),imp(drs([Y],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])
-    )
-).
-
-% or, typed
+%type on discourse referent
 test(
     merge(
         drs([Y:e],[]),
         app(lam(X,
-            drs([],[eq(Y,X),or(drs([],[pred(happy,Y)]),drs([],[pred(sad,Y)]))])),
+            drs([],[eq(X,Y),pred(sleep,Y)])),
         mary)
     ),
     merge(
         drs([Y:e],[]),
-        drs([],[eq(Y,mary),or(drs([],[pred(happy,Y)]),drs([],[pred(sad,Y)]))])
+        drs([],[eq(mary,Y),pred(sleep,Y)])
     )
 ).
 
-% not, typed discourse referent
-test(
-    merge(
-        drs([Y:e],[]),
-        app(lam(X,
-            drs([],[eq(Y,X),not(drs([],[pred(musician,Y)]))])),
-        john)
-    ),
-    merge(
-        drs([Y:e],[]),
-        drs([],[eq(Y,john),not(drs([],[pred(musician,Y)]))])
-    )
-).
-
-% not, typed elsewhere
-test(
-    merge(
-        drs([Y],[]),
-        app(lam(X:e,
-            drs([],[eq(Y,X:e),not(drs([],[pred(musician,Y)]))])),
-        john:e)
-    ),
-    merge(
-        drs([Y],[]),
-        drs([],[eq(Y,john:e),not(drs([],[pred(musician,Y)]))])
-    )
-).
+%% some, typed
+%test(
+%    merge(
+%        drs([X:e,Y:e],[]),
+%        app(lam(Z,
+%            drs([],[eq(X,Z),pred(musician,Y),rel(loves,X,Y)])),
+%        john)
+%    ),
+%    merge(
+%        drs([X:e,Y:e],[]),
+%        drs([],[eq(X,john),pred(musician,Y),rel(loves,X,Y)])
+%    )
+%).
+%
+%% forall, typed
+%test(
+%    merge(
+%        drs([X:e],[]),
+%        app(lam(Z,
+%            drs([],[eq(X,Z),imp(drs([Y:e],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])),
+%        john)
+%    ),
+%    merge(
+%        drs([X:e],[]),
+%        drs([],[eq(X,john),imp(drs([Y:e],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])
+%    )
+%).
+%
+%% forall, untyped
+%test(
+%    merge(
+%        drs([X],[]),
+%        app(lam(Z,
+%            drs([],[eq(X,Z),imp(drs([Y],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])),
+%        john)
+%    ),
+%    merge(
+%        drs([X],[]),
+%        drs([],[eq(X,john),imp(drs([Y],[pred(troll,Y)]),drs([],[rel(loves,X,Y)]))])
+%    )
+%).
+%
+%% or, typed
+%test(
+%    merge(
+%        drs([Y:e],[]),
+%        app(lam(X,
+%            drs([],[eq(Y,X),or(drs([],[pred(happy,Y)]),drs([],[pred(sad,Y)]))])),
+%        mary)
+%    ),
+%    merge(
+%        drs([Y:e],[]),
+%        drs([],[eq(Y,mary),or(drs([],[pred(happy,Y)]),drs([],[pred(sad,Y)]))])
+%    )
+%).
+%
+%% not, typed discourse referent
+%test(
+%    merge(
+%        drs([Y:e],[]),
+%        app(lam(X,
+%            drs([],[eq(Y,X),not(drs([],[pred(musician,Y)]))])),
+%        john)
+%    ),
+%    merge(
+%        drs([Y:e],[]),
+%        drs([],[eq(Y,john),not(drs([],[pred(musician,Y)]))])
+%    )
+%).
+%
+%% not, typed elsewhere
+%test(
+%    merge(
+%        drs([Y],[]),
+%        app(lam(X:e,
+%            drs([],[eq(Y,X:e),not(drs([],[pred(musician,Y)]))])),
+%        john:e)
+%    ),
+%    merge(
+%        drs([Y],[]),
+%        drs([],[eq(Y,john:e),not(drs([],[pred(musician,Y)]))])
+%    )
+%).
